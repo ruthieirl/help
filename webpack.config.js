c446abd82d5d3a7c05ddd25b0701614056e08014
ruthieirl <ruthfromjersey@gmail.com>
2018-01-02 20:30:13 -0500

    css module loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,35 @@ var config = getConfig({
   in: join(__dirname, 'src/app.js'),
   out: join(__dirname, 'dist'),
   clearBeforeBuild: true
+});
+
+// CSS loader
+
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l && l.loader && l.loader.match(match))
+  return found ? found[0] : null;
+}
+// existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
 })
+config.module.loaders.push(newloader);
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+});
 
 module.exports = config;
