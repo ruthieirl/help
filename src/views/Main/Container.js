0e98bc64bffc822b0430279779dd30b3886d7a48
ruthieirl <ruthfromjersey@gmail.com>
2018-01-04 19:42:58 -0500

    home route ok

---
+++ src/views/Main/Container.js
@@ -0,0 +1,14 @@
+// in src/views/Main/Container.js
+import React from 'react'
+
+export class Container extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello from the container
+      </div>
+    )
+  }
+}
+
+export default Container
