d09e7e361ff9142e9cb9a0e8bb618f7a2d34d9ca
ruthieirl <ruthfromjersey@gmail.com>
2018-01-04 20:15:02 -0500

    map working

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,14 +1,19 @@
 // in src/views/Main/Container.js
 import React from 'react'
+import Map, {GoogleApiWrapper} from 'google-maps-react'
 
 export class Container extends React.Component {
   render() {
     return (
       <div>
         Hello from the container
+        <Map
+          google={this.props.google} />
       </div>
     )
   }
 }
 
-export default Container
+export default GoogleApiWrapper({
+  apiKey: __GAPI_KEY__
+})(Container)
