cd3c50917ccf285ee0ea492e9ce4aec216a8a0c3
ruthieirl <ruthfromjersey@gmail.com>
2018-01-06 11:47:17 -0500

    list and map with prop err

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -2,17 +2,51 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
 
+import {searchNearby} from 'utils /googleApiHelpers'
+
 import Header from 'components/Header/Header'
 import Footer from 'components/Footer/Footer'
 
 export class Container extends React.Component {
+    constructor(props) {
+    super(props);
+
+    this.state = {
+      places: [],
+      pagination: null
+    }
+  }
+  onReady(mapProps, map) {
+    const {google} = this.props;
+    const opts = {
+      location: map.center,
+      radius: '500',
+      types: ['cafe']
+    }
+    searchNearby(google, map, opts)
+      .then((results, pagination) => {
+        this.setState({
+          places: results,
+          pagination
+        })
+      }).catch((status, result) => {
+        // There was an error
+      })
+  }
   render() {
     return (
       <div>
         <Header />
-          <Map
-          google={this.props.google} />
-        <Footer />
+        <Map
+          google={this.props.google}
+          onReady={this.onReady.bind(this)}
+          visible={false}>
+
+          {this.state.places.map(place => {
+            return (<div key={place.id}>{place.name}</div>)
+          })}
+
+        </Map>
       </div>
     )
   }
