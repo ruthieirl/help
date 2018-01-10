1bf74ef2c4eb7835a6aff33887fcd586c8b4d1a1
William Rainaud <wrainaud@gmail.com>
2018-01-10 09:08:24 -0500

    No Errors

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import {browserHistory, Router, Route, Redirect, IndexRoute} from 'react-router'
+import React from 'react'
+import { browserHistory, Router, Route, Redirect } from 'react-router'
 
-import makeMainRoutes from './views/Main/routes';
+import makeMainRoutes from './views/Main/routes'
 
 export const makeRoutes = () => {
   const main = makeMainRoutes();
 
   return (
-    <Route path="" component={Container}>
-      <Route path="map" component={Map} />
-      <Route path="detail/:placeId" component={Detail} />
-
-      <IndexRoute component={Map} />
+    <Route path=''>
+      {main}
     </Route>
   )
-};
+}
 
-export default makeRoutes;
+
+
+export default makeRoutes
--- src/views/Main/routes.js
+++ src/views/Main/routes.js
@@ -7,7 +7,7 @@ import Detail from './Detail/Detail';
 
 export const makeMainRoutes = () => {
   return (
-    <Route path="index" component={Container}>
+    <Route path="/" component={Container}>
       <Route path="/map" component={Map}>
         <Route path="/detail/:placeId"
           component={Detail} />
